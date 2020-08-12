d9fffa761afc6e644e4a9005f491e42024e1b266
DaoDucDev <daoducdev@gmail.com>
2020-08-12 20:17:13 +0700

    Struct

--- SS10/DemoString.c
+++ SS10/DemoString.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
@@ -9,12 +9,26 @@ int main(int argc, char const *argv[])
     //Nhập xuất string
     //Dùng printf() và scanf()
     //Dùng gets() và puts()
-
+    // int a;
+    // printf("a = ");
+    // scanf("%d", &a);
     printf("Nhap ten cua ban: ");
     // scanf("%[^\n]s", str);
+    fflush(stdin);
     gets(str);
-    printf("Xin chao %s", str);
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == 32)
+        {
+            for (int j = i; j < strlen(str); j++)
+            {
+                str[j] = str[j + 1];
+                i--;
+            }
+        }
+    }    
 
+    printf("Xin chao: ", str);
 
     return 0;
 }
