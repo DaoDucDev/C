48f0768ac66f0345c788d90782c59d4eae528fe4
DaoDucDev <daoducdev@gmail.com>
2021-11-08 15:20:36 +0700

    Update

--- KiemTraNamNhuan/KiemTraNamNhuan.c
+++ KiemTraNamNhuan/KiemTraNamNhuan.c
@@ -7,6 +7,11 @@ int main(int argc, char const *argv[])
     printf("Nhap so nam bat ki: ");
     scanf("%d", &nam);
 
+    // nam % 4 == 0 -> năm chia hết cho 4
+    // nam % 4 != 0 -> năm không chia hết cho 4
+    // nam % 400 == 0 -> năm chia hết cho 400
+    // &&: toán tử quan hệ VÀ (AND)
+    // ||: toán tử quan hệ HOẶC (OR)
     if((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0))
     {
         printf("%d la nam nhuan.", nam);
